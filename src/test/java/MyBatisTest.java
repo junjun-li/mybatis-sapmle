0e01eafb21fe54d24dfb66eafa098438e724cfff
lijunjun <11776174@qq.com>
2023-02-02 20:47:23 +0800

    feat: SqlSessionFactory的基本使用

---
+++ src/test/java/MyBatisTest.java
@@ -0,0 +1,35 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.Reader;
+import java.sql.Connection;
+
+public class MyBatisTest {
+    @Test
+    public void testSqlSessionFactory() throws Exception {
+        // 利用Reader加载classpath下的mybatis-config.xml核心配置文件
+        // AsReader: 按照字符流的方式进行读取
+        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+        // 初始化sqlSessionFactory对象解析xml文件
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        SqlSession sqlSession = null;
+        try {
+            // 创建SqlSession对象,这是JDBC的扩展类,用于与数据库交互
+            sqlSession = sqlSessionFactory.openSession();
+            Connection connection = sqlSession.getConnection();
+            System.out.println(connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (sqlSession != null) {
+                // 如果type="POOLED",代表使用连接池,close则是将连接回收到连接池中
+                // 如果type="UNPOOLED",代表直连,close则会调用Connection.close()方法关闭连接
+                sqlSession.close();
+            }
+        }
+    }
+}
